389360d0a48aec187739891fee00e49b4605c9dc
Joao vitor <joaovitorkilson@gmail.com>
2022-09-09 00:42:07 -0300

    add section why us

--- js/script.js
+++ js/script.js
@@ -26,3 +26,34 @@ function toggleBtnMobile() {
 }
 
 btnMobile.addEventListener('click', toggleBtnMobile)
+
+const whyUsCard = document.querySelectorAll('.card-list li')
+
+function handleHoverCard(event) {
+  const card = event.target
+  const childrenCards = Array.from(event.srcElement.children)
+  
+  card.classList.add('hover')
+  childrenCards.forEach((item) => {
+    item.classList.add('hover')
+  })
+  
+  console.log(card, childrenCards)
+
+
+}
+function handleOutHoverCard(event) {
+  const card = event.target
+  const childrenCards = Array.from(event.srcElement.children)
+  
+  card.classList.remove('hover')
+  childrenCards.forEach((item) => {
+    item.classList.remove('hover')
+  })
+}
+
+whyUsCard.forEach((item) => {
+  item.addEventListener('mouseenter', handleHoverCard)
+  item.addEventListener('mouseout', handleOutHoverCard)
+
+})
