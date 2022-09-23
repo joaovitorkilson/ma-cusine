cc0518487b02f3289ca564d57dfd7a5629adb8f4
Joao vitor <joaovitorkilson@gmail.com>
2022-09-23 15:59:28 -0300

    Done Why Us section

--- js/script.js
+++ js/script.js
@@ -1,59 +1,11 @@
-const btnMobile = document.querySelector('#btn-mobile')
-const nav = document.querySelector('nav')
- 
-function toggleMenu() {
-  nav.classList.toggle('active')
-}
-btnMobile.addEventListener('click', toggleMenu)
+import hamburgerMenu from './global/hamburgerMenu.js'
+
+import whyUsCard from './sections/whyUs.js'
 
 
-function resizeScreen() {
-  const screen = matchMedia('(min-width: 1000px)').matches
-  if (screen) {
-      if (nav.classList.contains('active')) {
-        nav.classList.remove('active')
+// Global
+hamburgerMenu()
 
-      }
-  }
-}
+// Sections 
 
-window.addEventListener('resize', resizeScreen)
-
-function toggleBtnMobile() {
-
-      btnMobile.classList.toggle('open')
-
-}
-
-btnMobile.addEventListener('click', toggleBtnMobile)
-
-const whyUsCard = document.querySelectorAll('.card-list li')
-
-function handleHoverCard(event) {
-  const card = event.target
-  const childrenCards = Array.from(event.srcElement.children)
-  
-  card.classList.add('hover')
-  childrenCards.forEach((item) => {
-    item.classList.add('hover')
-  })
-  
-  console.log(card, childrenCards)
-
-
-}
-function handleOutHoverCard(event) {
-  const card = event.target
-  const childrenCards = Array.from(event.srcElement.children)
-  
-  card.classList.remove('hover')
-  childrenCards.forEach((item) => {
-    item.classList.remove('hover')
-  })
-}
-
-whyUsCard.forEach((item) => {
-  item.addEventListener('mouseenter', handleHoverCard)
-  item.addEventListener('mouseout', handleOutHoverCard)
-
-})
+whyUsCard()
